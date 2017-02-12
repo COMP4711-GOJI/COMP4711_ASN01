ba718cdfcdc235b1f5a18efd514792c5f669b4e4
eyyu <eva.yw.yu@gmail.com>
2017-02-12 14:51:45 -0800

    EY:Made assemply view grid dynamically source robots and parts

--- application/controllers/Assembly.php
+++ application/controllers/Assembly.php
@@ -15,15 +15,35 @@ class Assembly extends Application
 	 */
 	public function index()
 	{
-		// this is the view we want shown
 		$this->data['pagebody'] = 'assembly';
-		//$source = $this->robots->all();
-		//$robotimg = array ();
-		/*foreach ($source as $record)
-		{
-			$robotimg[] = array ('img' => $record['img']);
-		}*/
-		//$this->data['img'] = $robotimg;
+		$robots = $this->robots->all();
+		$parts = $this->inventory->all_parts();
+		
+		$gallery = '<div class="row">';
+		foreach ($robots as $gridblock) {
+			$gallery .= '<div class="col-lg-4 col-sm-6 col-xs-12">';
+			$gallery .= '<a href="#">';
+			$gallery .= '<img src="./assets/images/bots/';
+			$gallery .= $gridblock['img'];
+			$gallery .= '" class="thumbnail img-responsive">';
+			$gallery .= '</a>';
+			$gallery .= '<button type="button" class="btn btn-primary btn-radio">Sell</button>';
+			$gallery .= '<input type="checkbox" id="sell-robot-button" class="hidden">';
+			$gallery .= '</div>';
+		}
+		$gallery .= '</div>';	
+		foreach ($parts as $gridblock) {
+			$gallery .= '<div class="col-lg-4 col-sm-6 col-xs-12">';
+			$gallery .= '<a href="#">';
+			$gallery .= '<img src="./assets/images/parts/';
+			$gallery .= $gridblock['image'];
+			$gallery .= '" class="thumbnail img-responsive">';
+			$gallery .= '</a>';
+			$gallery .= '<button type="button" class="btn btn-primary btn-radio">Sell</button>';
+			$gallery .= '<input type="checkbox" id="sell-robot-button" class="hidden">';
+			$gallery .= '</div>';
+		}
+		$this->data['assembly_gallery'] = $gallery;
 		$this->render();
 	}
 
--- application/views/assembly.php
+++ application/views/assembly.php
@@ -1,55 +1,6 @@
-<!--	Original Snipper of Code Accredited to : http://bootsnipp.com/snippets/7N3p2 !-->
 
-		  <h3>Assembly</h3>
-			<!-- tabs -->
-	<!-- Nav tabs -->
- <div class="row">
-    <div class="col-lg-4 col-sm-6 col-xs-12">
-        <a href="#">
-             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
-        </a>
-    </div>
-     <div class="col-lg-4 col-sm-6 col-xs-12">
-        <a href="#">
-             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
-        </a>
-    </div>
-     <div class="col-lg-4 col-sm-6 col-xs-12">
-        <a href="#">
-             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
-        </a>
-    </div>
-     <div class="col-lg-4 col-sm-6 col-xs-12">
-        <a href="#">
-             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
-        </a>
-    </div>
-     <div class="col-lg-4 col-sm-6 col-xs-12">
-        <a href="#">
-             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
-        </a>
-    </div>
-     <div class="col-lg-4 col-sm-6 col-xs-12">
-        <a href="#">
-             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
-        </a>
-    </div>
-     <div class="col-lg-4 col-sm-6 col-xs-12">
-        <a href="#">
-             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
-        </a>
-    </div>
-     <div class="col-lg-4 col-sm-6 col-xs-12">
-        <a href="#">
-             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
-        </a>
-    </div>
-     <div class="col-lg-4 col-sm-6 col-xs-12">
-        <a href="#">
-             <img src="http://placehold.it/800x600" class="thumbnail img-responsive">
-        </a>
-    </div>
-  </div>
-
-
+<h3>Assembly</h3>
+<div>
+{assembly_gallery}
+</div> 
 
