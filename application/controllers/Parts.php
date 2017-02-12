468fd76ea6565c1e0e382f513380614313f78f13
brodymccrone <brodymccrone21@gmail.com>
2017-02-12 11:12:20 -0800

    Create controller for robot parts.

--- application/controllers/Parts.php
+++ application/controllers/Parts.php
@@ -10,15 +10,49 @@ class Parts extends Application
 		parent::__construct();
 	}
 
-	/**
-	 * Homepage for our app
-	 */
-	public function index()
-	{
-		// this is the view we want shown
-		$this->data['pagebody'] = 'parts';
+    /**
+     * Parts page for our app
+     */
+    public function index()
+    {
+        // this is the view we want shown
+        $this->data['pagebody'] = 'parts';
 
-		$this->render();
-	}
+        $inv_parts = $this->inventory->all_parts();
+        /*
+        usort($parts, function ($item1, $item2) {
+            return $item2['id'] < $item1['id'];
+        });
+        */
 
+        $print = '<table>';
+        $print .= '<tr>';
+        $print .= '<th>Model</th>';
+        $print .= '<th>Line</th>';
+        $print .= '<th>Image</th>';
+        $print .= '</tr>';
+        foreach ($inv_parts as $item) {
+            $print .= '<tr>';
+            $print .= '<td>' . 'ADD MODEL' .'</td>';
+            $print .= '<td>' . 'ADD LINE' . '</td>';
+            $print .= '</tr>';
+            $print .= '<td>';
+            $print .= '<a href="./part/';
+            $print .= $item['id'];
+            $print .= '">';
+            $print .= '<img src="assets/images/parts/';
+            $print .= $item['image'];
+            $print .= '"></img>';
+            $print .= '</br>details...';
+            $print .= '</a>';
+            $print .= '</td>';
+            $print .= '</tr>';
+        }
+
+        $print .= "</table>";
+
+        $this->data['parts_table'] = $print;
+
+        $this->render();
+    }
 }
